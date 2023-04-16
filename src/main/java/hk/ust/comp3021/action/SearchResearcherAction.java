7418d71a52a316dbb6900f3c6870f1c046cdf0c7
Omdaniels <126331610+Omdaniels@users.noreply.github.com>
2023-04-16 22:38:44 +0800

    Update SearchResearcherAction.java

--- src/main/java/hk/ust/comp3021/action/SearchResearcherAction.java
+++ src/main/java/hk/ust/comp3021/action/SearchResearcherAction.java
@@ -68,7 +68,22 @@ public class SearchResearcherAction extends Action {
      * @param null
      * @return `actionResult` that contains the relevant researchers
      */
-    public Supplier<HashMap<String, List<Paper>>> searchFunc1;
+    public Supplier<HashMap<String, List<Paper>>> searchFunc1 = () -> {
+        int Y = 1; 
+        HashMap<String, List<Paper>> result = new HashMap<>();
+        for (Map.Entry<String, List<Paper>> entry : actionResult.entrySet()) {
+            String researcher = entry.getKey();
+            List<Paper> papers = entry.getValue();
+            for (Paper paper : papers) {
+                if (paper.getYear() == (new Date().getYear() + 1900) - Y) { 
+                    result.put(researcher, papers);
+                    break; 
+                }
+            }
+        }
+        return result;
+    };
+
 
     /**
      * TODO `searchFunc2` indicates the second searching criterion,
@@ -76,7 +91,23 @@ public class SearchResearcherAction extends Action {
      * @param null
      * @return `actionResult` that contains the relevant researchers
      */
-    public Supplier<HashMap<String, List<Paper>>> searchFunc2;
+    public Supplier<HashMap<String, List<Paper>>> searchFunc2 = () -> {
+        HashMap<String, List<Paper>> relevantResearchers = new HashMap<>();
+        for (Map.Entry<String, List<Paper>> entry : actionResult.entrySet()) {
+            List<Paper> papers = entry.getValue();
+            List<Paper> relevantPapers = new ArrayList<>();
+            for (Paper paper : papers) {
+                if (paper.getJournal().equals(searchFactorX) && paper.getAbsContent().length() >= Integer.parseInt(searchFactorY)) {
+                    relevantPapers.add(paper);
+                }
+            }
+            if (!relevantPapers.isEmpty()) {
+                relevantResearchers.put(entry.getKey(), relevantPapers);
+            }
+        }
+        return relevantResearchers;
+    };
+
 
     public static int getLevenshteinDistance(String str1, String str2) {
         return 0;
@@ -96,6 +127,61 @@ public class SearchResearcherAction extends Action {
      *     similarity like `(1 - levenshteinDistance / max(S1.length, S2.length)) * 100`.
      *     2) Note that we need to remove paper(s) from the paper list of whoever are co-authors with the given researcher.
      */
-    public Supplier<HashMap<String, List<Paper>>> searchFunc3;
+    
+    private int calculateLevenshteinDistance(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    
+
+    public Supplier<HashMap<String, List<Paper>>> searchFunc3 = () -> {
+        int percentageX = Integer.parseInt(this.searchFactorX);
+        String researcherY = this.searchFactorY;
+        List<Paper> papersResearcherY = new ArrayList<>();
+        papersResearcherY.addAll(this.actionResult.get(researcherY));
+
+        this.actionResult.entrySet().forEach(entry -> {
+            List<Paper> listPaper = entry.getValue();
+
+            List<Paper> listPaperFiltered = listPaper.stream()
+                    .filter(paper -> !paper.getAuthors().contains(researcherY)) // Remove papers where researcherY is a co-author
+                    .filter(paper -> {
+                        String keywordsCurrentPaper = paper.getKeywords().stream().collect(Collectors.joining(","));
+                        boolean isSimilar = papersResearcherY.stream()
+                                .anyMatch(paperResearcherY -> {
+                                    String keywordsPaperResearcherY = paperResearcherY.getKeywords().stream().collect(Collectors.joining(","));
+                                    int similarity = calculateLevenshteinDistance(keywordsCurrentPaper, keywordsPaperResearcherY);
+                                    int maxLength = Math.max(keywordsCurrentPaper.length(), keywordsPaperResearcherY.length());
+                                    return (1 - (double) similarity / maxLength) * 100 >= percentageX;
+                                });
+                        return isSimilar;
+                    })
+                    .collect(Collectors.toList());
+
+            listPaper.clear();
+            listPaper.addAll(listPaperFiltered);
+        });
+
+        this.actionResult.entrySet().removeIf(entry -> entry.getValue().size() == 0);
+        return this.actionResult;
+    };
+
 
 }
