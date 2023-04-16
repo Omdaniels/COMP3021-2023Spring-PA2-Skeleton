bbc13e2ac96031396b2b3c68e1e82a86e22aaa16
Omdaniels <126331610+Omdaniels@users.noreply.github.com>
2023-04-16 22:58:44 +0800

    Update CustomArrayList.java

--- src/main/java/hk/ust/comp3021/utils/CustomArrayList.java
+++ src/main/java/hk/ust/comp3021/utils/CustomArrayList.java
@@ -6,74 +6,89 @@ public class CustomArrayList<E> {
     private int capacity;
 
     /**
-     * TODO `CustomArrayList` constructor with default `capacity`(5)
-     * PS: `size` is set to the initial value 0
+     * CustomArrayList constructor with default capacity (5)
+     * `size` is set to the initial value 0
      */
     public CustomArrayList() {
-
+        capacity = 5;
+        elements = new Object[capacity];
     }
 
     /**
-     * TODO `CustomArrayList` constructor with given `capacity`
-     * PS: `size` is set to the initial value 0
+     * CustomArrayList constructor with given capacity
+     * `size` is set to the initial value 0
+     * @param initialCapacity the initial capacity of the list
      */
     public CustomArrayList(int initialCapacity) {
-
+        capacity = initialCapacity;
+        elements = new Object[capacity];
     }
 
     /**
-     * TODO `add` appends new element into `elements`. Once `size` is equal to `capacity`,
-     *       we need to resize `elements` to twice its original size.
+     * Adds a new element into `elements`. Once `size` is equal to `capacity`,
+     * we need to resize `elements` to twice its original size.
      * @param element to be added into `elements`
-     * @return null
      */
     public void add(E element) {
-
+        if (size == capacity) {
+            resize(capacity * 2);
+        }
+        elements[size++] = element;
     }
 
     /**
-     * TODO `resize` modifies the size of `elements`
+     * Modifies the size of `elements`
      * @param newCapacity to indicate the new capacity of `elements`
-     * @return null
      */
     private void resize(int newCapacity) {
-
+        Object[] newElements = new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[i];
+        }
+        elements = newElements;
+        capacity = newCapacity;
     }
 
     /**
-     * TODO `get` obtains target element based on the given index. Once the index is not within [0, size),
-     *       we need to return null.
+     * Obtains target element based on the given index. Once the index is not within [0, size),
+     * we need to return null.
      * @param index to indicate the element position
-     * @return element whose index is `index`
+     * @return element whose index is `index`, or null if index is out of range
      */
     public E get(int index) {
-        return (E) new Object();
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return (E) elements[index];
     }
 
     /**
-     * TODO `size` obtains the size of `elements`
-     * @param null
+     * Obtains the size of `elements`
      * @return `size`
      */
     public int size() {
-        return 0;
+        return size;
     }
 
     /**
-     * TODO `isEmpty` determine whether the list is empty
-     * @param null
+     * Determines whether the list is empty
      * @return boolean variable that indicates the list status
      */
     public boolean isEmpty() {
-        return true;
+        return size == 0;
     }
 
     /**
-     * TODO `contains` determine whether the input is in `elements`
+     * Determines whether the input is in `elements`
      * @param obj to be determined
      * @return boolean variable that indicates the existence of `obj`
      */
     public boolean contains(E obj) {
-        return true;
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(obj)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
